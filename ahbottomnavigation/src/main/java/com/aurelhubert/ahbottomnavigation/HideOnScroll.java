c6b18e6a800b570d814bcd679fa2427f4769d77f
Aurelien Hubert <aurel.hubert@gmail.com>
2016-03-18 10:47:14 +0100

    Added HideOnScroll utility

---
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/HideOnScroll.java
@@ -0,0 +1,115 @@
+package com.aurelhubert.ahbottomnavigation;
+
+/**
+ * Created by Michele on 18/03/2016.
+ *
+ * This is HideOnScroll utility made for AHBottomNavigation.
+ *
+ * Copyright 2016 Michele Lacorte
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v7.widget.RecyclerView;
+import android.view.animation.LinearInterpolator;
+
+public final class HideOnScroll {
+
+    public static class ShowHideBottomBarOnScrollingListener extends RecyclerView.OnScrollListener {
+
+        private AHBottomNavigation bottomBar;
+        private State state;
+
+        public ShowHideBottomBarOnScrollingListener(AHBottomNavigation bottomBar) {
+            this.bottomBar = bottomBar;
+            this.state = new State();
+        }
+
+
+        /**
+         * Bottom bar animation show
+         */
+        private void bottomBarAnimateShow() {
+            bottomBar.animate()
+                    .translationY(0)
+                    .setInterpolator(new LinearInterpolator())
+                    .setDuration(50);
+        }
+
+        /**
+         * Bottom bar animation hide
+         */
+        private void bottomBarAnimateHide() {
+            bottomBar.animate()
+                    .translationY(bottomBar.getHeight())
+                    .setInterpolator(new LinearInterpolator())
+                    .setDuration(50);
+        }
+
+        @Override
+        public final void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                if (state.scrollingOffset > 0) {
+                    bottomBarAnimateHide();
+                } else if (state.scrollingOffset < 0) {
+                    bottomBarAnimateShow();
+                }
+            }
+        }
+
+        @Override
+        public final void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            state.scrollingOffset = dy;
+            bottomBar.animate().cancel();
+            if (dy > 0) {
+                bottomBarAnimateHide();
+            } else if (dy < 0) {
+                bottomBarAnimateShow();
+            }
+        }
+
+        /**
+         * Saving and restoring current state.
+         */
+        public static final class State implements Parcelable {
+            public static Creator<State> CREATOR = new Creator<State>() {
+                public State createFromParcel(Parcel parcel) {
+                    return new State(parcel);
+                }
+
+                public State[] newArray(int size) {
+                    return new State[size];
+                }
+            };
+            // Scroll UP/DOWN offset
+            private int scrollingOffset;
+
+            State() {
+            }
+
+            State(Parcel parcel) {
+                this.scrollingOffset = parcel.readInt();
+            }
+
+            @Override
+            public int describeContents() {
+                return 0;
+            }
+
+            @Override
+            public void writeToParcel(Parcel parcel, int flags) {
+                parcel.writeInt(scrollingOffset);
+            }
+        }
+    }
+}
---
+++ demo/src/main/java/aurelhubert/com/ahbottomnavigation/ItemData.java
@@ -0,0 +1,19 @@
+package aurelhubert.com.ahbottomnavigation;
+
+/**
+ * Created by Michele on 18/03/2016.
+ */
+public class ItemData {
+
+
+    private String title;
+    public ItemData(String title){
+
+        this.title = title;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+}
